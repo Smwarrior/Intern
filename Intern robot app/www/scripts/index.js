1cf5859f7952543a7e3cd5123768e5bf5100f0f0
Smwarrior <nonsens_4@hotmail.com>
2017-03-14 10:25:05 +0100

    jtyjt

--- Intern robot app/www/scripts/index.js
+++ Intern robot app/www/scripts/index.js
@@ -15,7 +15,7 @@
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        var receivedElement = parentElement.querySelector('.received'); 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
     };
